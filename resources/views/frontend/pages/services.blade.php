56507e1a6dc535d870ae1b5a1199a8eeaee85dd8
ignacio1997img+ <ignaciomolinaguzman20@gmail.com>
2022-06-18 22:01:22 -0400

    Reserva

--- resources/views/frontend/pages/services.blade.php
+++ resources/views/frontend/pages/services.blade.php
@@ -37,7 +37,7 @@
         <div class="container" data-aos="fade-up">
 
             <div class="section-title">
-            <h2>SERVICIOSsss</h2>
+            <h2>SERVICIOS</h2>
             <h3>¿Qué <span>Necesitás Hacer?</span></h3>
             </div>
 
