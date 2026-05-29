7d703a58aa97f27566335c56d3b7843005fa3c1b
nachodevos <ignaciomolinaguzman20gmail.com>
2026-05-29 11:49:09 -0400

    Actualizando los campos para registrar cargos adicionales

--- resources/views/additionaljob/browse.blade.php
+++ resources/views/additionaljob/browse.blade.php
@@ -12,12 +12,8 @@
                         <div class="panel-body" style="padding: 0px">
                             <div class="col-md-8" style="padding: 0px">
                                 <h1 class="page-title">
-                                <i class="voyager-pen"></i> Cargos Adicionales
+                                    <i class="voyager-pen"></i> Cargos Adicionales
                                 </h1>
-                                {{-- <div class="alert alert-info">
-                                    <strong>Información:</strong>
-                                    <p>Puede obtener el valor de cada parámetro en cualquier lugar de su sitio llamando <code>setting('group.key')</code></p>
-                                </div> --}}
                             </div>
                             @if(auth()->user()->hasPermission('add_additional_jobs'))
                                 <div class="col-md-4 text-right" style="margin-top: 30px">
@@ -41,43 +37,43 @@
                     <div class="panel panel-bordered">
                         <div class="panel-body">
                             <div class="table-responsive">
-                                <table id="dataTable" class="dataTable table-hover">
-                                    <table style="width:100%"  class="table dataTable table-bordered table-striped table-sm">
+                                <table style="width:100%" class="dataTable table table-bordered table-striped table-hover table-sm">
                                     <thead>
                                         <tr>
                                             <th>Id&deg;</th>
                                             <th>Funcionario</th>
                                             <th>Cargo</th>
+                                            <th>Observación</th>
                                             <th>Estado</th>
                                             <th></th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                            @foreach ($data as $item)
-                                                <tr>
-                                                    <td>{{$item->id}}</td>
-                                                    <td>{{ optional($item->person)->first_name }} {{ optional($item->person)->last_name }}</td>
-                                                    <td>{{$item->cargo}}</td>
-                                                    <td>
-                                                        @if ($item->status == 0)
-                                                            <label class="label label-danger">Inactivo</label>
-                                                        @else
-                                                            <label class="label label-success">Activo</label>
-                                                        @endif
-                                                    </td>
-                                                    <th style="text-align: right">
-                                                        @if ($item->status == 1)
-                                                            <a href="" data-toggle="modal" data-target="#modalBaja" data-id="{{$item->id}}" class="btn btn-sm btn-warning">
-                                                                <i class="voyager-edit"></i> <span>Baja</span>
-                                                            </a>
-                                                            <a href="" data-toggle="modal" data-target="#modalDelete" data-id="{{$item->id}}" class="btn btn-sm btn-danger">
-                                                                <i class="voyager-trash"></i> <span>Eliminar</span>
-                                                            </a>
-                                                        @endif
-
-                                                    </th>
-                                                </tr>
-                                            @endforeach
+                                        @foreach ($data as $item)
+                                            <tr>
+                                                <td>{{ $item->id }}</td>
+                                                <td>{{ optional($item->person)->first_name }} {{ optional($item->person)->paternal_surname }} {{ optional($item->person)->maternal_surname }}</td>
+                                                <td>{{ $item->cargo }}</td>
+                                                <td>{{ $item->observacion ?? '—' }}</td>
+                                                <td>
+                                                    @if ($item->status == 0)
+                                                        <label class="label label-danger">Inactivo</label>
+                                                    @else
+                                                        <label class="label label-success">Activo</label>
+                                                    @endif
+                                                </td>
+                                                <td class="text-right">
+                                                    @if ($item->status == 1)
+                                                        <a href="" data-toggle="modal" data-target="#modalBaja" data-id="{{ $item->id }}" class="btn btn-sm btn-warning">
+                                                            <i class="voyager-edit"></i> <span>Baja</span>
+                                                        </a>
+                                                        <a href="" data-toggle="modal" data-target="#modalDelete" data-id="{{ $item->id }}" class="btn btn-sm btn-danger">
+                                                            <i class="voyager-trash"></i> <span>Eliminar</span>
+                                                        </a>
+                                                    @endif
+                                                </td>
+                                            </tr>
+                                        @endforeach
                                     </tbody>
                                 </table>
                             </div>
@@ -86,18 +82,16 @@
                 </div>
             </div>
         </div>
-        <!-- The Modal -->
+
+        <!-- Modal Registrar -->
         <div class="modal fade" role="dialog" id="modalRegistrar">
             <div class="modal-dialog">
                 <div class="modal-content">
-
-                    <!-- Modal Header -->
                     <div class="modal-header btn-success">
-                        <h4 class="modal-title"><i class="voyager-plus"></i>Registrar Cargos Adicionales</h4>
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title"><i class="voyager-plus"></i> Registrar Cargos Adicionales</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">&times;</button>
                     </div>
-                    {!! Form::open(['route' => 'additional_jobs.store','class' => 'was-validated'])!!}
-                    <!-- Modal body -->
+                    {!! Form::open(['route' => 'additional_jobs.store', 'class' => 'was-validated']) !!}
                     <div class="modal-body">
                         <div class="row">
                             <div class="col-sm-12">
@@ -106,9 +100,9 @@
                                         <span class="input-group-text"><b>Persona:</b></span>
                                     </div>
                                     <select name="person_id" class="form-control select2" required>
-                                        <option value="">Seleccione una opcion</option>
-                                        @foreach($people as $data)
-                                            <option value="{{$data->id}}">{{$data->ci}} - {{$data->first_name}} {{$data->paternal_surname}} {{$data->maternal_surname}}</option>
+                                        <option value="">Seleccione una opción</option>
+                                        @foreach($people as $person)
+                                            <option value="{{ $person->id }}">{{ $person->ci }} - {{ $person->first_name }} {{ $person->paternal_surname }} {{ $person->maternal_surname }}</option>
                                         @endforeach
                                     </select>
                                 </div>
@@ -117,106 +111,83 @@
                         <div class="row">
                             <div class="col-md-12">
                                 <div class="input-group-prepend">
-                                <span class="input-group-text"><b>Cargo:</b></span>
+                                    <span class="input-group-text"><b>Cargo:</b></span>
                                 </div>
                                 <input type="text" class="form-control" id="cargo" name="cargo" required>
                             </div>
                         </div>
-                        <div class="row">
+                        <div class="row" style="margin-top: 10px">
                             <div class="col-md-12">
                                 <div class="input-group-prepend">
-                                <span class="input-group-text"><b>Observación:</b></span>
+                                    <span class="input-group-text"><b>Observación:</b></span>
                                 </div>
                                 <textarea name="observacion" id="observacion" class="form-control" rows="3"></textarea>
                             </div>
                         </div>
-
-
                     </div>
-
-                    <!-- Modal footer -->
                     <div class="modal-footer justify-content-between">
-                        <button type="button text-left" class="btn btn-danger" data-dismiss="modal" data-toggle="tooltip" title="Volver">Cancelar
-                        </button>
-                        <button type="submit" class="btn btn-success btn-sm" title="Registrar..">
-                            Registrar
-                        </button>
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+                        <button type="submit" class="btn btn-success btn-sm">Registrar</button>
                     </div>
-                    {!! Form::close()!!}
-
+                    {!! Form::close() !!}
                 </div>
             </div>
         </div>
 
-
+        <!-- Modal Baja -->
         <div class="modal fade" role="dialog" id="modalBaja">
             <div class="modal-dialog">
                 <div class="modal-content">
-
-                    <!-- Modal Header -->
                     <div class="modal-header btn-warning">
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
                         <h4 class="modal-title"><i class="voyager-edit"></i> Dar de Baja</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">&times;</button>
                     </div>
-                    {!! Form::open(['route' => 'additional_jobs.baja', 'class' => 'was-validated'])!!}
-                    <!-- Modal body -->
+                    {!! Form::open(['route' => 'additional_jobs.baja', 'class' => 'was-validated']) !!}
                     <div class="modal-body">
-                        <input type="hidden" name="id" id="id">
-                        <div class="text-center" style="text-transform:uppercase">
+                        <input type="hidden" name="id" id="baja_id">
+                        <div class="text-center" style="text-transform: uppercase">
                             <i class="voyager-warning" style="color: orange; font-size: 5em;"></i>
                             <br>
-                            <p><b>Dar de Baja....!</b></p>
+                            <p><b>¿Dar de Baja este registro?</b></p>
                         </div>
-
-
                     </div>
-
-                    <!-- Modal footer -->
                     <div class="modal-footer justify-content-between">
-                        <button type="button text-left" class="btn btn-danger" data-dismiss="modal" data-toggle="tooltip" title="Volver">Cancelar
-                        </button>
-                        <button type="submit" class="btn btn-warning btn-sm" title="Baja..">
-                            Baja
-                        </button>
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+                        <button type="submit" class="btn btn-warning btn-sm">Confirmar Baja</button>
                     </div>
-                    {!! Form::close()!!}
-
+                    {!! Form::close() !!}
                 </div>
             </div>
         </div>
 
+        <!-- Modal Eliminar -->
         <div class="modal fade modal-danger" tabindex="-1" id="modalDelete" role="dialog">
             <div class="modal-dialog">
                 <div class="modal-content">
                     {!! Form::open(['route' => 'additional_jobs.delete', 'method' => 'DELETE']) !!}
                     <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar"><span aria-hidden="true">&times;</span></button>
                         <h4 class="modal-title"><i class="voyager-trash"></i> Eliminar</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar"><span aria-hidden="true">&times;</span></button>
                     </div>
                     <div class="modal-body">
-                        <input type="hidden" name="id" id="id">
-
-                        <div class="text-center" style="text-transform:uppercase">
+                        <input type="hidden" name="id" id="delete_id">
+                        <div class="text-center" style="text-transform: uppercase">
                             <i class="voyager-trash" style="color: red; font-size: 5em;"></i>
                             <br>
-
-                            <p><b>Desea eliminar el siguiente registro?</b></p>
+                            <p><b>¿Desea eliminar este registro?</b></p>
                         </div>
                     </div>
                     <div class="modal-footer">
-
-                            <input type="submit" class="btn btn-danger pull-right delete-confirm" value="Sí, eliminar">
-
                         <button type="button" class="btn btn-default pull-right" data-dismiss="modal">Cancelar</button>
+                        <input type="submit" class="btn btn-danger pull-right delete-confirm" value="Sí, eliminar">
                     </div>
-                    {!! Form::close()!!}
+                    {!! Form::close() !!}
                 </div>
             </div>
         </div>
     @stop
 
     @section('css')
-
     @stop
 
     @section('javascript')
@@ -225,7 +196,6 @@
             $(document).ready(() => {
                 $('.dataTable').DataTable({
                     language: {
-                        // "order": [[ 0, "desc" ]],
                         sProcessing: "Procesando...",
                         sLengthMenu: "Mostrar _MENU_ registros",
                         sZeroRecords: "No se encontraron resultados",
@@ -243,8 +213,8 @@
                             sPrevious: "Anterior"
                         },
                         oAria: {
-                            "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
-                            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                            sSortAscending: ": Activar para ordenar la columna de manera ascendente",
+                            sSortDescending: ": Activar para ordenar la columna de manera descendente"
                         },
                         buttons: {
                             copy: "Copiar",
@@ -252,33 +222,18 @@
                         }
                     },
                     order: [[ 0, 'desc' ]],
-                })
+                });
             });
 
             $('#modalBaja').on('show.bs.modal', function (event) {
-                // alert('hola');
-                var button = $(event.relatedTarget)
-
-                var id = button.data('id')
-
-                var modal = $(this)
-                modal.find('.modal-body #id').val(id)
-
+                var id = $(event.relatedTarget).data('id');
+                $(this).find('#baja_id').val(id);
             });
 
             $('#modalDelete').on('show.bs.modal', function (event) {
-                // alert('hola');
-                var button = $(event.relatedTarget)
-
-                var id = button.data('id')
-                // alert(34)
-
-                var modal = $(this)
-                modal.find('.modal-body #id').val(id)
-
+                var id = $(event.relatedTarget).data('id');
+                $(this).find('#delete_id').val(id);
             });
-
-
         </script>
     @stop
 
